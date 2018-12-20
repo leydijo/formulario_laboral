c1ef308b8db2e43f5ac6e67bf28f081c5a3d7d8e
leydijo <leydijohana182010@hotmail.com>
2018-12-20 17:52:17 -0300

    continuacion de la horas

--- calcularhoras.php
+++ calcularhoras.php
@@ -3,10 +3,12 @@
 $mysqli = new Mysqli('localhost', 'root', 'S0p0rt32019', 'alt');
 $mysqli->set_charset("utf8");
 
-$t=mysqli_query($mysqli, "SELECT HOUR(SUM(TIMEDIFF(ingreso, egreso))) FROM actividades ");	
+$t=mysqli_query($mysqli, "SELECT HOUR(SUM(TIMEDIFF(salida_almuerzo, regreso_almuerzo))) FROM actividades  ");	
 
 while($f = $t->fetch_array()){
     echo $f[0] ;
+    echo "horas almuerzo";
+       
 }
 
 ?>
