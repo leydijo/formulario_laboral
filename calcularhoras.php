01ef8417ec8b5f5b7185d2d7e024e1033eb27ca4
leydijo <leydijohana182010@hotmail.com>
2018-12-20 17:15:47 -0300

    realiza operacion de mysql en php

--- calcularhoras.php
+++ calcularhoras.php
@@ -3,10 +3,10 @@
 $mysqli = new Mysqli('localhost', 'root', 'S0p0rt32019', 'alt');
 $mysqli->set_charset("utf8");
 
-$t=mysqli_query($mysqli, "SELECT HOUR(SUM(TIMEDIFF(ingreso, egreso))) FROM actividades WHERE id=13");	
+$t=mysqli_query($mysqli, "SELECT HOUR(SUM(TIMEDIFF(ingreso, egreso))) FROM actividades ");	
 
 while($f = $t->fetch_array()){
-    echo $f["ingreso"] ;
+    echo $f[0] ;
 }
 
 ?>
