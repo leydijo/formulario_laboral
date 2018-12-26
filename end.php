35e73d2d7cb4f69a48c42283b434bf272f4fdc78
leydijo <leydijohana182010@hotmail.com>
2018-12-25 21:04:41 -0600

    suma horas

--- end.php
+++ end.php
@@ -4,7 +4,8 @@
   && isset($_POST['cliente']) && !empty($_POST['cliente']) && isset($_POST['fecha']) && !empty($_POST['fecha'])
   && isset($_POST['homework']) && !empty($_POST['homework']) && isset($_POST['descripcion']) && !empty($_POST['descripcion'])
   && isset($_POST['ingreso']) && !empty($_POST['ingreso'])  && isset($_POST['exit']) && !empty($_POST['exit'])
-  && isset($_POST['regreso']) && !empty($_POST['regreso'])  && isset($_POST['egreso']) && !empty($_POST['egreso'])){
+  && isset($_POST['regreso']) && !empty($_POST['regreso'])  && isset($_POST['egreso']) && !empty($_POST['egreso'])
+  ){
 
     $con=mysqli_connect($host,$user,$pw,$db)or die("Problemas al conectar");
 
@@ -12,11 +13,13 @@
     cliente =('$_POST[cliente]'),
     fecha_registro = ('$_POST[fecha]'), tareas_realizadas = ('$_POST[homework]'), descripcion = ('$_POST[descripcion]'), 
     ingreso = ('$_POST[ingreso]'), salida_almuerzo =('$_POST[exit]'), regreso_almuerzo = ('$_POST[regreso]'), 
-    egreso = ('$_POST[egreso]')");
+    egreso = ('$_POST[egreso]'),totalhoras = TIMEDIFF(('$_POST[egreso]'),('$_POST[ingreso]'))");
     
     echo "datos insertado";
-    
-	}else {
+
+   
+         
+  }else {
 	  echo "no se ha podido guardar los datos";
   }
  
