4695f9781ab1a9b16191abd3f4bd53396496e67a
leydijo <leydijohana182010@hotmail.com>
2018-12-28 16:32:08 -0300

    parte logica terminado

--- consulta.php
+++ consulta.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 include('inicio.php');// CONEXION A LA BD
 
 $fecha1=$_POST['fecha1'];
@@ -8,35 +8,38 @@ $apellido=$_POST['apellido'];
 
 if(isset($_POST['generar_reporte']))
 {
-	// NOMBRE DEL ARCHIVO Y CHARSET
-	header('Content-Type:text/csv; charset=latin1');
-	header('Content-Disposition: attachment; filename="Reporte_Mes_Laboral.csv"');
+    // NOMBRE DEL ARCHIVO Y CHARSET
+    header('Content-Type:text/csv; charset=latin1');
+    header('Content-Disposition: attachment; filename="Reporte_Mes_Laboral.csv"');
 
-	// SALIDA DEL ARCHIVO
-	$salida=fopen('php://output', 'w');
-	// ENCABEZADOS las columnas del excel
-	fputcsv($salida, array('Identificador', 'Nombre', 'Apellido', 'Cliente','ingreso','egreso','Horasdias','totalMes'));
-	// QUERY PARA CREAR EL REPORTE
-	$sumaCsv=$conexion->query("SELECT fecha_regitro SEC_TO_TIME(SUM(TIME_TO_SEC(horasdias))) as hours FROM actividades WHERE (nombre = '$nombre')AND(apellido = '$apellido')ORDER BY id");
-	$reporteCsv=$conexion->query("SELECT * FROM actividades WHERE (nombre = '$nombre')AND(apellido = '$apellido') ORDER BY id ");
-
-
-	while(($filaR=$reporteCsv->fetch_assoc())
-&&($fila=$sumaCsv->fetch_assoc()))
-		fputcsv($salida, array($filaR['id'], 
-								$filaR['nombre'],
-								$filaR['apellido'],
-								$filaR['cliente'],
-								$filaR['ingreso'],
-								$filaR['egreso'],
-								$filaR['horasdias'],
+    // SALIDA DEL ARCHIVO
+    $salida=fopen('php://output', 'w');
+    // ENCABEZADOS las columnas del excel
+    fputcsv($salida, array('Identificador', 'Fecha', 'Nombre', 'Apellido', 'Cliente','ingreso','egreso','Horasdias'));
+    // QUERY PARA CREAR EL REPORTE
+    $sumaCsv=$conexion->query("SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(horasdias))) AS hours FROM actividades WHERE 
+	(fecha_registro BETWEEN '$fecha1' AND '$fecha2')AND(nombre = '$nombre')AND(apellido = '$apellido')");
+	
+    $reporteCsv=$conexion->query("SELECT * FROM actividades WHERE ((fecha_registro BETWEEN '$fecha1' AND '$fecha2')AND
+    (nombre = '$nombre')AND(apellido = '$apellido')) ORDER BY id ");
+    
+    while($filaR=$reporteCsv->fetch_assoc()){
+        fputcsv($salida, array($filaR['id'],
+								$filaR['fecha_registro'],		
+                                $filaR['nombre'],
+                                $filaR['apellido'],
+                                $filaR['cliente'],
+                                $filaR['ingreso'],
+                                $filaR['egreso'],
+                                $filaR['horasdias']));
+									
+	}
+	if($fila=$sumaCsv->fetch_assoc())
+        fputcsv($salida, array('', '', '', '', '','','TOTAL',
 								$fila['hours']));
-							
-				
+	
 }
 
 
+
 ?>
-
-
-
--- formu.js
+++ formu.js
@@ -1,24 +1,9 @@
-var name = $("#name").val();
 
 
-$('#next').on("click", function (e){
-    e.preventDefault();
-    $('#principal').addClass("hidden");
-    $('.mainContainer').removeClass("hidden");
-});
+    $(function() {
+        
+         $("#datepicker").datepicker({ beforeShowDay: $.datepicker.noWeekends }) 
+       
+     });
 
-// (function(){
-//     $('#formulario').validate({
-//         submitHandler: function(form) {
-//             $(form).ajaxSubmit({
-//                 url: 'send.php',
-//                 success: function() {
-                    
-                    
-//                     $('#formulario').hide();
-//                     $('#tprincipal').append("<p class='thanks'>Gracias por contactar con nosotros.</p>")
-//                 }
-//             });
-//         }
-//     });         
-// });
+     
